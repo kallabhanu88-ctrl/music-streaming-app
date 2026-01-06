21e88152d4d3d0e7a05b073572230e1ff12d5341
Keerthi Dasari <keerthidasari@Keerthis-MacBook-Air.local>
2026-01-06 11:31:55 +0530

    Add main.py for Flask app

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///music.db"
     app.config['JWT_SECRET_KEY'] = "super-secret-key"
     app.config['UPLOAD_FOLDER'] = "uploads"
@@ -29,7 +31,8 @@ def create_app():
 
     return app
 
-app = create_app()
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5001, debug=True)
